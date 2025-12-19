2d87caa701fdb52bf5e635517a7b3df53341dca7
DatTM <dattm2k3@gmail.com>
2025-12-19 20:39:27 +0700

    Tài xỉu realtime + animation

--- server.js
+++ server.js
@@ -5,14 +5,29 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-io.on('connection', socket => {
-    console.log('Client connected:', socket.id);
+io.on("connection", socket => {
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
+    console.log("User joined:", socket.id);
+
+    socket.on("roll-dice", () => {
+
+        // random xúc xắc server → sync toàn bộ client
+        let d1 = Math.floor(Math.random()*6) + 1;
+        let d2 = Math.floor(Math.random()*6) + 1;
+        let d3 = Math.floor(Math.random()*6) + 1;
+
+        io.emit("dice-result", {
+            d1, d2, d3,
+            total: d1 + d2 + d3
+        });
     });
+
+    socket.on("disconnect", () => {
+        console.log("User left:", socket.id);
+    });
+
 });
 
 http.listen(process.env.PORT || 3000, () => {
-    console.log('Server đang chạy');
+    console.log('Server đang chạy...');
 });
